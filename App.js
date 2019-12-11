d8d823bcfc6eb0d847006369d5a61e31e8c9f3a4
Julie Erlemeier <jmerlemeier@gmail.com>
2019-12-10 18:45:51 -0800

    created styles with stylesheet.

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera'; 
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Camera App</Text>
+    <View style={{ flex: 1 }}>
+      <Text>Welcome to your Camera</Text>
+      {/* <Button 
+        title="Get Camera"
+        onPress=""
+        /> */}
+      <Camera style={{ flex: 1 }} type={type}>
+        <View
+          style={styles.view}>
+          <TouchableOpacity
+            style={styles.touchableOpacity}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 20, marginBottom: 12, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  touchableOpacity: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
     justifyContent: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+# Camera App with React Native 
+
+### Docs
+[Expo](https://docs.expo.io/versions/latest/sdk/camera/)
